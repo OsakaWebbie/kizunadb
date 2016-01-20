6acab2911c7410f5621ce7ef58c0b6a0cdd2cb08
Karen Ellrick <karen@l4jp.com>
2016-01-20 21:30:18 +0900

    Defaults for "kanji numbers" and "post office stamp" set when format is selected. Also, jQuery calls moved to bottom of code and code indents improved.

--- ms_printaddr.php
+++ ms_printaddr.php
@@ -4,8 +4,6 @@ include("accesscontrol.php");
 header1("");
 ?>
 <link rel="stylesheet" href="style.php?jquery=1" type="text/css" />
-<script type="text/JavaScript" src="js/jquery.js"></script>
-<script type="text/JavaScript" src="js/jquery-ui.js"></script>
 <?
 header2(0);
 
@@ -37,37 +35,50 @@ $sql = "SELECT count(DISTINCT h.HouseholdID) FROM person p LEFT JOIN household h
 $result = sqlquery_checked($sql);
 $num_households = mysql_result($result,0);
 ?>
-    <h3><?=_("Select options for address printing and click the button.")?></h3>
-    <form action="print_addr.php" method="post" name="optionsform" target="_blank" style="text-align:left">
-      <input type="hidden" name="pid_list" value="<? echo $pid_list; ?>" border="0">
-      <div style="display:inline-block;vertical-align:middle;margin:0 2em">
-        <input type="radio" name="name_type" value="ind" tabindex="1" border="0"><?=_("Individuals")." (".$num_individuals.")"?><br />
-        <input type="radio" name="name_type" value="label" border="0" checked><?=_("Households")." (".$num_households.")"?>
-      </div>
-      <div style="display:inline-block;vertical-align:middle">
-        <label class="label-n-input"><?=_("Envelope/Postcard Format")?>: <select name="addr_print_name" size="1">
+<h3><?=_("Select options for address printing and click the button.")?></h3>
+<form action="print_addr.php" method="post" name="optionsform" target="_blank" style="text-align:left">
+  <input type="hidden" name="pid_list" value="<? echo $pid_list; ?>" border="0">
+  <div style="display:inline-block;vertical-align:middle;margin:0 2em">
+    <input type="radio" name="name_type" value="ind" tabindex="1" border="0"><?=_("Individuals")." (".$num_individuals.")"?><br />
+    <input type="radio" name="name_type" value="label" border="0" checked><?=_("Households")." (".$num_households.")"?>
+  </div>
+  <div style="display:inline-block;vertical-align:middle">
+    <label class="label-n-input"><?=_("Envelope/Postcard Format")?>: <select id="addrprint-select" name="addr_print_name" size="1">
 <?
-$result = sqlquery_checked("SELECT AddrPrintName FROM addrprint ORDER BY ListOrder,AddrPrintName");
+$result = sqlquery_checked("SELECT AddrPrintName,Tategaki,DefaultStamp FROM addrprint ORDER BY ListOrder,AddrPrintName");
 while ($row = mysql_fetch_object($result)) {
-  echo  "                  <option value=\"".$row->AddrPrintName."\">".$row->AddrPrintName."</option>\n";
+  echo  "      <option value=\"".$row->AddrPrintName."\" data-kanjinumbers=\"".$row->Tategaki."\"".
+        " data-stamp=\"".$row->DefaultStamp."\">".$row->AddrPrintName."</option>\n";
 }
 ?>
-        </select></label><br>
-        <div style="display:inline-block;vertical-align:middle;margin:0 2em">
-          <label class="label-n-input"><input type="checkbox" value="yes" name="nj_separate" checked><?=_("Sort by Japan/foreign")?></label><br />
-          <label class="label-n-input"><input type="checkbox" value="yes" name="kanji_numbers" checked><?=_("Use kanji for numbers")?></label>
-        </div>
-        <div style="display:inline-block;vertical-align:middle;margin:0 2em">
-          <h4><?=_("Post office stamp:")?></h4>
-          <input type="radio" name="po_stamp" value="none" checked><?=_("None")?><br />
-          <input type="radio" name="po_stamp" value="betsunou"><?=_("Standard mail")?><br />
-          <input type="radio" name="po_stamp" value="yuumail_betsunou"><?=_("'Yuu-mail'")?><br />
-          <input type="radio" name="po_stamp" value="kounou"><?=_("Standard mail w/ contract")?><br />
-          <input type="radio" name="po_stamp" value="yuumail_kounou"><?=_("'Yuu-mail' w/ contract")?>
-        </div>
-      </div>
-      <input type="submit" name="submit" value="<?=_("Make PDF")?>" border="0">
-    </form>
+    </select></label><br>
+    <div style="display:inline-block;vertical-align:middle;margin:0 2em">
+      <label class="label-n-input"><input type="checkbox" value="yes" name="nj_separate" checked><?=_("Sort by Japan/foreign")?></label><br />
+      <label class="label-n-input"><input type="checkbox" value="yes" name="kanji_numbers" id="kanji_numbers" checked><?=_("Use kanji for numbers")?></label>
+    </div>
+    <div style="display:inline-block;vertical-align:middle;margin:0 2em">
+      <h4><?=_("Post office stamp:")?></h4>
+      <input type="radio" name="po_stamp" value="none" checked><?=_("None")?><br />
+      <input type="radio" name="po_stamp" value="betsunou"><?=_("Standard mail")?><br />
+      <input type="radio" name="po_stamp" value="yuumail_betsunou"><?=_("'Yuu-mail'")?><br />
+      <input type="radio" name="po_stamp" value="kounou"><?=_("Standard mail w/ contract")?><br />
+      <input type="radio" name="po_stamp" value="yuumail_kounou"><?=_("'Yuu-mail' w/ contract")?>
+    </div>
+  </div>
+  <input type="submit" name="submit" value="<?=_("Make PDF")?>" border="0">
+</form>
+
+<script type="text/JavaScript" src="js/jquery.js"></script>
+<script type="text/JavaScript" src="js/jquery-ui.js"></script>
+<script type="text/JavaScript">
+$(document).ready(function() {
+  $('#addrprint-select').change(function() {
+    $('#kanji_numbers').prop('checked',$('#addrprint-select option:selected').data("kanjinumbers")==1);
+    $('input:radio[name=po_stamp]').filter('[value='+$('#addrprint-select option:selected').data("stamp")+']').prop('checked', true);
+  });
+  $('#addrprint-select').trigger('change');
+});
+</script>
   <? footer();
 ?>
 
