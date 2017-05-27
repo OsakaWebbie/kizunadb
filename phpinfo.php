1f280ed183d099b6c280191c1340f6bc4b1c573f
osakawebbie <osakawebbie@gmail.com>
2017-05-27 13:51:41 +0900

    Allow dev user to see PHP Info

--- phpinfo.php
+++ phpinfo.php
@@ -1,7 +1,7 @@
 <?php
 include("functions.php");
 include("accesscontrol.php");
-if ($_SESSION['userid']=="karen") {
+if ($_SESSION['userid']=="karen" || $_SESSION['userid']=="dev") {
   phpinfo();
 }
 ?>
