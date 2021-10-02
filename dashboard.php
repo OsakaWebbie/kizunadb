e704510de437e23fab924a53e04c70b86e0a385a
osakawebbie <osakawebbie@gmail.com>
2021-10-02 21:32:03 +0900

    Made dashboard capable of using files for modules instead of code in DB. Accompanying DB change: add user.Dashboard column. (Need more development later to allow users to select modules, but this addresses the security risk.) Also, old cleanup to pullin_postals.

--- dashboard.php
+++ dashboard.php
@@ -3,16 +3,16 @@ include("functions.php");
 include("accesscontrol.php");
 if ($_SESSION['admin'] && isset($_GET['user'])) {  /* to test or view other user's dashboards */
   $user = $_GET['user'];
-  $sql = "SELECT UserName,DashboardCode FROM user WHERE UserID='$user'";
-  $result = sqlquery_checked("SELECT UserName,DashboardCode FROM user WHERE UserID='$user'");
+  $result = sqlquery_checked("SELECT UserName,Dashboard,DashboardCode FROM user WHERE UserID='$user'");
   ($row = mysqli_fetch_object($result)) || die("No record found for user '$user'.");
-  $hasdashboard = ($row->DashboardCode != '');
+
+  $hasdashboard = ($row->Dashboard != '' || $row->DashboardCode != '');
   $username = $row->UserName;
 } else {
   $user = $_SESSION['userid'];
   $hasdashboard = $_SESSION['hasdashboard'];
   if ($hasdashboard) {
-    $result = sqlquery_checked("SELECT DashboardCode FROM user WHERE UserID='".$_SESSION['userid']."'");
+    $result = sqlquery_checked("SELECT Dashboard,DashboardCode FROM user WHERE UserID='".$_SESSION['userid']."'");
     $row = mysqli_fetch_object($result);
     $username = $_SESSION['username'];
   }
@@ -28,10 +28,21 @@ if (!$hasdashboard) {
   echo "<h3>"._("You don't have a dashboard yet.  If you would like one, talk to your KizunaDB administrator.")."</h3>\n";
   footer();
 } else {
-  //eval($row->DashboardHead);
   header2(1);
-  echo "<h1 id='title'>".$_SESSION['dbtitle'].": ".$username._("'s Dashboard")."</h1>\n";
-  eval($row->DashboardCode);
+  echo "<h1 id='title'>" . $_SESSION['dbtitle'] . ": " . $username . _("'s Dashboard") . "</h1>\n";
+  if ($row->Dashboard != '') {
+    $files = explode(',',$row->Dashboard);
+    foreach ($files as $file) {
+      //$file = CLIENT_PATH.'/dashboard/'.$file.'.php';
+      if (file_exists(CLIENT_PATH.'/dashboard/'.$file.'.php')) {
+        include(CLIENT_PATH.'/dashboard/'.$file.'.php');
+      } else {
+        echo '<div>Dashboard file'.$file.'.php was not found.</div>';
+      }
+    }
+  } else {
+    eval($row->DashboardCode);
+  }
   echo "<div style='clear:both'></div>";
   footer();
 }
