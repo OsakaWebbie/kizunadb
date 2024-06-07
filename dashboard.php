d9ab6ccf118196edd2d730b1063dde49c9016a27
osakawebbie <osakawebbie@gmail.com>
2024-06-07 19:58:34 +0900

    1. print_addr.php: added custom file capability 2. individual.php: made donations and pledges tables sortable 3. edit.php: fixed postalcode fill-in behavior bugs 4. dashboard.php: fixed minor bug when oncall enabled but no file 5. get_postalcode.php: cosmetic

--- dashboard.php
+++ dashboard.php
@@ -3,11 +3,14 @@ include("functions.php");
 include("accesscontrol.php");
 
 if (!empty($_GET['oncall'])) {  // code to only be run when specifically requested
-  if (strpos($_SESSION['dashboard_oncall'],$_GET['oncall']) !== FALSE
-      && file_exists(CLIENT_PATH . '/dashboard/' . $_GET['oncall'] . '.php')) {
-    include(CLIENT_PATH . '/dashboard/' . $_GET['oncall'] . '.php');
-  } else echo '<div>On-call file "' . $_GET['oncall'] . '.php" was not found.<br>Path: '.CLIENT_PATH . '/dashboard/' . $_GET['oncall'] . '.php</div>';
-  exit;
+  if (strpos($_SESSION['dashboard_oncall'],$_GET['oncall']) !== FALSE) {
+    if (file_exists(CLIENT_PATH . '/dashboard/' . $_GET['oncall'] . '.php')) {
+      include(CLIENT_PATH . '/dashboard/' . $_GET['oncall'] . '.php');
+    } else {
+      echo '<div>On-call file "' . $_GET['oncall'] . '.php" was not found.<br>Path: '.CLIENT_PATH . '/dashboard/' . $_GET['oncall'] . '.php</div>';
+    }
+    exit;
+  }
 }
 
 if ($_SESSION['admin'] && isset($_GET['user'])) {  /* to test or view other user's dashboards */
